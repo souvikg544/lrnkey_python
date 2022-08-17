a98a839b37ed77693de32bba8fdc812206f3aea7
souvikg544 <souvikg544@gmail.com>
2022-08-17 20:05:35 +0530

    Arithmatic and binary operators in Python

---
+++ simple_operations.py
@@ -0,0 +1,25 @@
+a= int(input("Enter first number : "))
+b= int(input("Enter second number : "))
+
+#Simple Addition
+add=a+b
+print("The addition of the numbers are",add)
+
+# Simple multiplication
+
+multiply=a*b
+print("The multiplacation of the numbers are",multiply)
+
+#Simple substraction and division
+
+# Finding out the remainder or the modulus
+
+r=7%3
+print("The remainder is",r)
+
+# Boolean operator
+# Boolean = True or False
+
+res= a>b
+
+print("Result is ", res)
