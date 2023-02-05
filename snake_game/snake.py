3614589e9f6ed78ba1afcbe3853134cd8e082bf0
souvikg544 <souvikg544@gmail.com>
2023-02-06 01:21:36 +0530

    Snake ganme with turtle

--- snake_game/snake.py
+++ snake_game/snake.py
@@ -8,7 +8,7 @@ delay = 100
 
 # (x,y)
 offset={
-    "up":(0,20),
+    "up":(0,20),   # (x,y)
     "down":(0,-20),
     "left":(-20,0),
     "right":(20,0)
@@ -16,8 +16,22 @@ offset={
 }
 # Space for definitions
 
-#Reset ---> move ---> collision -----> Food function ---->Distance between snake and food
+# Reset ---> move ---> collision -----> Food function ---->Distance between snake and food
+def reset():
+    global snake,snake_dir,food_position,pen
+    snake=[[0,0],[0,20],[0,40],[0,60],[0,80]]
+    snake_dir="up"
+    food_position=get_random_food_pos()
+    food.goto(food_position)
+    move_snake()
 
+def move_snake():
+    global snake_dir
+    new_head=snake[-1].copy()
+    new_head[0]=snake[-1][0]+ offset[snake_dir][0]
+    new_head[1] = snake[-1][1] + offset[snake_dir][1]
+
+    if new_head
 # Utilize all the functions below
 
 #Go up
